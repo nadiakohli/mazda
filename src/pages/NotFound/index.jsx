3ef3109c20d9fef192b1bee09f9d4711f304de90
nadiakohli <andruknadia@gmail.com>
2022-11-08 15:05:43 +0200

    add adaptive design on NotFound page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
-// Images
-import notFoundImg from 'assets/images/NotFound404.jpg';
-
 // Components
 import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
 import Button from 'components/common/Button';
 
 // Styles
-import { Wrap, ContentWrap, InfoWrap } from './styled';
+import {
+  Wrap,
+  ContentWrap,
+  InfoWrap,
+  ImageWrap,
+} from './styled';
 
 const NotFound = () => (
   <Wrap>
     <Header backgroundColor="#191919" />
     <ContentWrap>
-      <img src={notFoundImg} alt="not found" />
+      <ImageWrap>
+        <h1>Error 404</h1>
+      </ImageWrap>
       <InfoWrap>
-        <h1>Page not found</h1>
+        <h2>Page not found</h2>
         <p>Sorry, the page you want to open doesn&apos;t seem to exist.</p>
         <Button color="black" to="/">Home page</Button>
       </InfoWrap>
