040b63f770c02e9203ad37b8fd29703f75f32d19
nadiakohli <andruknadia@gmail.com>
2022-11-09 12:04:16 +0200

    add adaptive design on Home page

--- src/components/Home/Driving/styled.js
+++ src/components/Home/Driving/styled.js
@@ -4,22 +4,51 @@ import styled from 'styled-components';
 import mazdaDriving from 'assets/images/mazdaDriving.jpg';
 
 export const Wrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
   background-image: url(${mazdaDriving});
   height: 100vh;
   background-size: cover;
   background-position: 50%;
   background-repeat: no-repeat;
   padding: 0 135px;
-  position: relative;
+
+  @media screen and (max-width: 950px) {
+    padding: 0 75px;
+    background-position: 65%;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 0 50px;
+    background-position: 25%;
+    justify-content: center;
+  }
+
+  @media screen and (max-width: 480px) {
+    padding: 0 25px;
+    background-position: 30%;
+  }
 `;
 
 export const ContentWrap = styled.div`
   display: flex;
   flex-direction: column;
   width: 35%;
-  position: absolute;
-  bottom: 35px;
-  right: 75px;
+  margin-bottom: 35px;
+
+  @media screen and (max-width: 950px) {
+    width: 65%;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 80%;
+    justify-content: flex-end;
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 100%;
+  }
 
   & h2, span, p {
     color: ${({ theme: { colors: { monsoon } } }) => monsoon};
@@ -34,15 +63,43 @@ export const ContentWrap = styled.div`
 
   & h2 {
     font-size: 36px;
+
+    @media screen and (max-width: 950px) {
+      font-size: 28px;
+    }
+
+    @media screen and (max-width: 480px) {
+      font-size: 21px;
+    }
   }
 
   & span {
     font-size: 24px;
+
+    @media screen and (max-width: 950px) {
+      font-size: 21px;
+    }
+
+    @media screen and (max-width: 480px) {
+      font-size: 18px;
+    }
   }
 
   & p {
     line-height: 1.7;
     font-size: 14px;
     font-weight: 400;
+
+    @media screen and (max-width: 950px) {
+      font-size: 12px;
+    }
+
+    @media screen and (max-width: 768px) {
+      width: 95%;
+    }
+
+    @media screen and (max-width: 480px) {
+      width: 100%;
+    }
   }
 `;
--- src/components/common/Card/styled.js
+++ src/components/common/Card/styled.js
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
 
 export const Wrap = styled.div`
-  width: 33.4%;
+  width: 33.3%;
   height: 540px;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
 
   & img {
     width: 100%;
     height: 400px;
+    
+    @media screen and (max-width: 768px) {
+      height: 77%;
+    }
   }
 `;
 
@@ -14,10 +22,15 @@ export const DescriptionWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-evenly;
   height: 100px;
   border-right: ${({ borderRight }) => borderRight ? '3px solid #e3e3e3' : ''};
   margin: 20px 0;
+
+  @media screen and (max-width: 768px) {
+    border-right: none;
+    height: 70px;
+  }
 
   & h3, p {
     font-family: ${({ theme: { fonts: { ubuntu } } }) => ubuntu};
@@ -27,6 +40,18 @@ export const DescriptionWrap = styled.div`
     font-size: 21px;
     text-transform: uppercase;
     font-weight: 800;
+
+    @media screen and (max-width: 950px) {
+      font-size: 18px;
+    }
+
+    @media screen and (max-width: 768px) {
+      font-size: 21px;
+    }
+
+    @media screen and (max-width: 480px) {
+      font-size: 18px;
+    }
   }
 
   & p {
@@ -36,6 +61,20 @@ export const DescriptionWrap = styled.div`
     font-size: 14px;
     font-weight: 700;
     color: ${({ theme: { colors: { granite } } }) => granite};
+
+    @media screen and (max-width: 950px) {
+      font-size: 12px;
+    }
+
+    @media screen and (max-width: 768px) {
+      font-size: 14px;
+      width: 80%;
+    }
+
+    @media screen and (max-width: 480px) {
+      font-size: 12px;
+      width: 85%;
+    }
   }
 
   & img {
