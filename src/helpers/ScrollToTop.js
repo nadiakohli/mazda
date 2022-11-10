1f6d93e8bf2af91bf57350fb4d739c93183bcbd3
nadiakohli <andruknadia@gmail.com>
2022-11-10 14:42:04 +0200

    fix some issues

--- src/helpers/ScrollToTop.js
+++ src/helpers/ScrollToTop.js
@@ -2,15 +2,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const { pathname, key } = useLocation();
-
-  useEffect(() => {
-    document.documentElement.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
-  }, [key]);
+  const { pathname } = useLocation();
 
   useEffect(() => {
     document.documentElement.scrollTo({
